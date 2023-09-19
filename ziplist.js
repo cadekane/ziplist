82de431eabe5f145670d72000cedede4258c9e7e
cadekane <89505252+cadekane@users.noreply.github.com>
2023-09-18 16:14:11 -1000

    Finished 8:00

--- ziplist.js
+++ ziplist.js
@@ -1,20 +1,16 @@
-const arrOne = [1, 2, 3, 4];
-const arrTwo = [5, 6, 7, 8];
+const arrOne = [1, 2, 3];
+const arrTwo = [4, 5, 6];
 
-function zipList(listOne, listTwo) {
+function zipList(first, second) {
   const arr = [];
-  for (let i = 0; i < listOne.length; i++) {
-    arr.push(listOne[i]);
-    arr.push(listTwo[i]);
+  for (let i = 0; i < first.length; i++) {
+    arr.push(first[i], second[i]);
   }
   return arr;
 }
 
 console.log(zipList(arrOne, arrTwo));
 
-function zipListTheSimpleWay(listOne, listTwo) {
-  return _.zip(listOne, listTwo);
-}
+const zipListTheSimpleWay = (first, second) => _.flatten(_.zip(first, second));
 
 console.log(zipListTheSimpleWay(arrOne, arrTwo));
-// DNF
